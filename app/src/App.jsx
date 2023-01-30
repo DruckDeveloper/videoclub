31b1433be79857a748e568b994fb1af0201865d6
Lida Vega <lidavegalara@gmail.com>
2023-01-30 17:37:04 -0500

    Buildning backend route

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,33 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import "./App.css"
+import movie from "./apis";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [video, setVideo] = useState([]);
+
+  async function fetchData() {
+    const { data } = await movie.get("/movie");
+    setVideo([data]);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <div>ddd</div>
   )
 }
 
--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,12 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config({ path: "../.env" });
 
-const PORT = 3030;
+const PORT = process.env.PORT || 3030;
 const app = express();
 
 const videoRoutes = require("./routes/videoRoutes");
 //const indexRoute = require("./routes/indexRoute");
-const connectionOptions = { useUnifiedTopology: true, useNewUrlParser: true };
+const connectionOptions = { useUnifiedTopology: true, useNewUrlParser: true};
 
 app.use(express.json());
 app.use(cors());
@@ -16,11 +16,11 @@ app.use(cors());
 const dbuser = process.env.VITE_USER;
 const dbpass = process.env.VITE_PASS;
 
-mongoose.connect(`mongodb+srv://${dbuser}:${dbpass}@videoclub.n2m4frn.mongodb.net/movie`, connectionOptions)
+mongoose.connect(`mongodb+srv://${dbuser}:${dbpass}@videoclub.n2m4frn.mongodb.net/videoclub?retryWrites=true&w=majority`, connectionOptions)
     .then(() => console.log("Connected successfully, with Atlass"))
     .catch((err) => console.error(err));
 
-app.use("/video", videoRoutes);
+app.use("/movie", videoRoutes);
 //app.use("/", indexRoute);
 
 app.listen(PORT, () => {
